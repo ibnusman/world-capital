5ace4a7089b88410e33a02aeb71cfcafb1db41f2
ibnusman <abusaleh3035@gmail.com>
2025-01-27 15:19:12 +0100

    feat: initial commit for World Capitals Quiz app

---
+++ index.js
@@ -0,0 +1,84 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+
+
+const app = express();
+const port = 3000;
+const pool = new pg.Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+pool.connect((err) => {
+  if (err) {
+    console.error("Failed to connect to database", err.stack);
+  } else {
+    console.log("Connected to the database");
+  }
+});
+
+
+
+
+let quiz = [
+  { country: "France", capital: "Paris" },
+  { country: "United Kingdom", capital: "London" },
+  { country: "United States of America", capital: "New York" },
+];
+async function nextQuestion() {
+  const result = await db.query("SELECT * FROM capitals ORDER BY RANDOM() LIMIT 1");
+  currentQuestion = result.rows[0];
+}
+
+
+let totalCorrect = 0;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let currentQuestion = {};
+
+// GET home page
+app.get("/", async (req, res) => {
+  totalCorrect = 0;
+  await nextQuestion();
+  console.log(currentQuestion);
+  res.render("index.ejs", { question: currentQuestion });
+});
+
+// POST a new post
+app.post("/submit", (req, res) => {
+  let answer = req.body.answer.trim();
+  let isCorrect = false;
+  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+    totalCorrect++;
+    console.log(totalCorrect);
+    isCorrect = true;
+  }
+
+  nextQuestion();
+  res.render("index.ejs", {
+    question: currentQuestion,
+    wasCorrect: isCorrect,
+    totalScore: totalCorrect,
+  });
+});
+
+async function nextQuestion() {
+  const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
+
+  currentQuestion = randomCountry;
+}
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
